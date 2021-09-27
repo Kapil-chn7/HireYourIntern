e269b10c9f9059203fb0d5b456a2544443a262d7
Kapil <kapilchn7@gmail.com>
2021-09-27 12:09:22 +0530

    another time

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app=express();
-const PORT=5000 || process.env.PORT;
+const PORT=process.env.PORT || 5000;
 const cors=require('cors')
 const bodyParser=require('body-parser')
 const cookieSession=require('cookie-session');
